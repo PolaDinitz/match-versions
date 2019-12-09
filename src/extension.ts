3035c769553e5e8e13b5d6f90326bc24c3255b6c
Pola Dinitz <poladinitz@gmail.com>
2019-12-09 21:12:55 +0200

    changed folders tree

--- src/extension.ts
+++ src/extension.ts
@@ -17,7 +17,7 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('bla bla!');
+		vscode.window.showInformationMessage('Hello World!');
 	});
 
 	context.subscriptions.push(disposable);
